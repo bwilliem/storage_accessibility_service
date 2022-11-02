c7e9d2dfa2099c1e847efe7a6cfdfaddaac77d74
bwilliem <beat.wil105@gmail.com>
2022-11-01 22:10:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/storage_accessibility_service/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.storage_accessibility_service
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.provider.Settings.SettingNotFoundException
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (!checkAccessibilityPermission()) {
+            Toast.makeText(this@MainActivity, "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // method to check is the user has permitted the accessibility permission
+    // if not then prompt user to the system's Settings activity
+    fun checkAccessibilityPermission(): Boolean {
+        var accessEnabled = 0
+        try {
+            accessEnabled =
+                Settings.Secure.getInt(this.contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED)
+        } catch (e: SettingNotFoundException) {
+            e.printStackTrace()
+        }
+        return if (accessEnabled == 0) {
+            // if not construct intent to request permission
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            // request permission via start activity for result
+            startActivity(intent)
+            false
+        } else {
+            true
+        }
+    }
+}
---
+++ app/src/main/java/com/example/storage_accessibility_service/StorageAccessibilityService.kt
@@ -0,0 +1,50 @@
+package com.example.storage_accessibility_service
+
+import android.accessibilityservice.AccessibilityService
+import android.util.Log
+import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
+
+
+
+
+
+class StorageAccessibilityService : AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent) {
+        when (event.eventType) {
+            AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> runSaf(event)
+        }
+    }
+
+    override fun onInterrupt() {
+
+    }
+
+    private fun runSaf(event: AccessibilityEvent) {
+        // Checks if the window open is SAF
+        if (event.packageName == "com.google.android.documentsui") {
+            // Gets the page XML
+            val currentNode = event.source
+
+            val checkedNode = currentNode.getChild(0).getChild(0).getChild(0).getChild(6).getChild(1)
+            if (checkedNode.className == "android.widget.Button" && checkedNode.text.toString() == "SAVE") {
+                checkedNode.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+
+//            saveButton.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            dfs(currentNode, "")
+//            Log.d("BeatriceLog", "currentNode: $currentNode")
+        }
+    }
+
+    fun dfs(info: AccessibilityNodeInfo?, level: String) {
+        if (info == null) return
+//        if (info.text != null && info.text.length > 0) Log.d("BeatriceLog", info.text.toString() + " class: " + info.className + " level: " + level )
+        if (info.text != null) Log.d("BeatriceLog", info.text.toString() + " class: " + info.className + " level: " + level )
+        for (i in 0 until info.childCount) {
+            val child = info.getChild(i)
+            dfs(child, "$level -> $i")
+            child?.recycle()
+        }
+    }
+}
